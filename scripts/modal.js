f97fe489b593abe05a9c89132f367d969e171c4f
FelipeKaua <fkauadelima@gmail.com>
2021-07-09 08:07:15 -0300

    dark mode

--- scripts/modal.js
+++ scripts/modal.js
@@ -14,10 +14,51 @@ function aplicarBackground(){
     const url =  document.getElementsByClassName("url")[0];
     const back = document.getElementsByClassName("background")[0];
     back.style.backgroundImage = "url(" + url.value + ")";
-    url.value = "";
 }
 function removerBackground(){
     const back = document.getElementsByClassName("background")[0];
     back.style.backgroundImage = "none";
     url.value = "";
+}
+function toggleVerify(){
+    const darkToggle = document.getElementsByClassName("checkbox")[0]
+    if(darkToggle.checked == true){
+       
+        dark();
+    }
+    else if(darkToggle.checked == false){
+     
+        dark();
+    }
+}
+function dark(){
+    // get
+    const background = document.getElementsByClassName("background")[0];
+    const estado = document.getElementsByClassName("estado")
+    const estadoText = document.getElementsByClassName("estadoText")
+    const modal = document.getElementsByClassName("modalContent")[0]
+    const input = document.getElementsByClassName("input")[0]
+    const textoModalSub = document.getElementsByClassName("textoModalSub")[0]
+    const textoModalSub2 = document.getElementsByClassName("textoModalSub2")[0]
+    const textoModal = document.getElementsByClassName("textoModal")[0]
+    const url = document.getElementsByClassName("url")[0]
+    const limpar = document.getElementsByClassName("limpar")[0]
+    const aplicar = document.getElementsByClassName("aplicar")[0]
+    // toggle
+    for(var i =0; i<estado.length;i++){
+        estado[i].classList.toggle("darkEstado");
+    }
+    for(var i =0; i<estadoText.length;i++){
+        estadoText[i].classList.toggle("darkEstadoText");
+    }
+    limpar.classList.toggle("darkLimpar")
+    aplicar.classList.toggle("darkAplicar")
+    url.classList.toggle("darkUrl")
+    textoModalSub2.classList.toggle("darkTextoModalSub2");
+    textoModalSub.classList.toggle("darkTextoModalSub");
+    modal.classList.toggle("darkModalContent")
+    textoModal.classList.toggle("darkTextoModal")
+    input.classList.toggle("darkInput")
+    background.classList.toggle("darkBackground");
+    
 }
